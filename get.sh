bf80e1bb3a59fc1f72ec1e338d8b7dbb63d14cae
陆盛涛 <3095471386@qq.com>
2021-12-21 13:48:54 +0800

    push

---
+++ get.sh
@@ -0,0 +1,2 @@
+// 拉取远程的仓库
+git clone git@github.com:lushengtao0115/museum.git master 
--- push.sh
+++ push.sh
@@ -2,4 +2,5 @@ git init
 git add .
 git commit -m 'push'
 
-git push -f git@github.com:lushengtao0115/museum.git master # 提交到这个地址的master分支
+# 提交到这个地址的master分支
+git push -f git@github.com:lushengtao0115/museum.git master 
